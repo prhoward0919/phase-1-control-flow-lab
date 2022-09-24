8229f4ee2ff24c9573c9edef01c7e2868d9e9c5a
Paige <prhoward97@gmail.com>
2022-09-24 11:26:26 -0400

    commit tree 45ea674fc5576a1fc1171403eafc67a29a881bcb

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let rideResponse;
+  if (ride <= 400) {rideResponse = "This one is on me!";
+  } else if (ride >= 401 && ride <= 2000) {rideResponse = "That will be twenty bucks.";
+  } else if (ride >= 2001 && ride <= 2500) {rideResponse = "I will gladly take your thirty bucks.";
+  } else if (ride >= 2501) {rideResponse = "No can do.";}
+  return rideResponse;
+}
+   
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  const isCity = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return isCity;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let thankfulness;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      thankfulness = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      thankfulness = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      thankfulness = 'Bye.';
+  }
+  return thankfulness;
 }
